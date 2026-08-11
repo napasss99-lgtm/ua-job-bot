93d5bbab1ebe87f6cc57148a2ce5934f55b4ff7a
napasss99-lgtm <napasss99@gmail.com>
2026-08-11 16:17:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,19 @@ def home():
     return "Бот працює 24/7!"
 
 def send_test_post():
+    print("Очікування 10 секунд перед відправкою...", flush=True)
     time.sleep(10)
     try:
         url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
-        photo_url = "https://raw.githubusercontent.com/napasss99-lgtm/ua-job-bot/main/Gemini_Generated_Image_bfrpo9bfrpo9bfrp.png"
+        photo_url = "https://raw.githubusercontent.com/napasss99-lgnm/ua-job-bot/main/Gemini_Generated_Image_bfrpo9bfrpo9bfrp.png"
         caption = "🔵 Бот успішно запущений і працює!"
         payload = {"chat_id": CHANNEL_NAME, "photo": photo_url, "caption": caption}
         
+        print("Надсилаємо запит до Telegram...", flush=True)
         response = requests.post(url, json=payload)
-        print("ВІДПОВІДЬ ВІД ТЕЛЕГРАМУ:", response.text)
+        print("ВІДПОВІДЬ ВІД ТЕЛЕГРАМУ:", response.text, flush=True)
     except Exception as e:
-        print("Помилка:", e)
+        print("ПОМИЛКА:", e, flush=True)
 
 if __name__ == "__main__":
     t = threading.Thread(target=send_test_post)
@@ -32,4 +34,3 @@ if __name__ == "__main__":
     t.start()
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
